bc49314a5df37b9b37d2e38842cb4f55582e2624
JeongAYoo <jeonga038@naver.com>
2022-11-21 23:14:04 +0900

    TutorialVC, Setting UI

--- weatherApp/TutorialViewController.swift
+++ weatherApp/TutorialViewController.swift
@@ -6,15 +6,122 @@
 //
 
 import UIKit
+import SnapKit
 
 class TutorialViewController: UIViewController {
-
+    
+    var images = ["mainMockUpCut", "searchMockUpCut"]
+    var titleText = ["위치 기반 날씨 정보", "지역 검색과 추가하기"]
+    var detailText = ["현재 위치한 곳의 날씨를 자동으로 표시합니다.\n위치 권한 허용이 필요합니다.", "지역을 검색하고, 나만의 리스트에 추가해 언제든 편리하게 날씨 정보를 확인하세요."]
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpView()
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(TutorialViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(TutorialViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
     }
     
-
-
+    func setUpView() {
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = 2
+        pageControl.pageIndicatorTintColor = .lightGray
+        pageControl.currentPageIndicatorTintColor = .black
+        
+        
+        doneButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(70)
+            make.right.equalToSuperview().offset(-20)
+        }
+        doneButton.setTitle("Skip", for: .normal)
+        
+        imgView.snp.makeConstraints { make in
+            make.left.equalTo(10)
+            make.right.equalTo(-10)
+            make.bottom.equalTo(pageControl.snp.top).offset(-20)
+            make.height.equalTo(350)
+        }
+        imgView.image = UIImage(named: "mainMockUpCut")
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(10)
+            make.right.equalTo(-10)
+            make.top.equalTo(doneButton.snp.bottom).offset(40)
+            
+        }
+        titleLabel.text = titleText[0]
+        titleLabel.sizeToFit()
+        titleLabel.textAlignment = .center
+        titleLabel.font = .systemFont(ofSize: 22, weight: .bold)
+        
+        detailLabel.snp.makeConstraints { make in
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
+            //make.height.equalTo(60)
+        }
+        
+        detailLabel.text = detailText[0]
+        detailLabel.sizeToFit()
+        detailLabel.textAlignment = .center
+        detailLabel.numberOfLines = 0
+        
+        pageControl.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-50)
+            make.centerX.equalToSuperview()
+        }
+        
+    }
+    
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//            pageControl.currentPage = Int(floor(scrollView.contentOffset.x / UIScreen.main.bounds.width))
+//    }
+    
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+    @IBAction func doneButtonClicked(_ sender: UIButton) {
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.left:
+                if (pageControl.currentPage<pageControl.numberOfPages-1) {
+                    pageControl.currentPage = pageControl.currentPage + 1
+                }
+                print("Swiped Left")
+            case UISwipeGestureRecognizer.Direction.right:
+                if (pageControl.currentPage>0) {
+                    pageControl.currentPage = pageControl.currentPage - 1
+                }
+                print("Swiped Right")
+            default:
+                break
+            }
+            
+            imgView.image = UIImage(named: images[pageControl.currentPage])
+            titleLabel.text = titleText[pageControl.currentPage]
+            detailLabel.text = detailText[pageControl.currentPage]
+            
+            if pageControl.currentPage == 1 {
+                doneButton.setTitle("완료", for: .normal)
+            } else {
+                doneButton.setTitle("Skip", for: .normal)
+            }
+        }
+    }
 }
